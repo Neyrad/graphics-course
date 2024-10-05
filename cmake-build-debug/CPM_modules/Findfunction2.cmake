5d215c5be4efa05496a81e299090bc4de157f0b0
Gritsaenko Andrey <gapandrey328@gmail.com>
2024-10-05 17:54:19 +0300

    init

---
+++ cmake-build-debug/CPM_modules/Findfunction2.cmake
@@ -0,0 +1,3 @@
+include("C:/Users/79014/CLionProjects/graphics-course/cmake-build-debug/cmake/CPM_0.40.2.cmake")
+CPMAddPackage("GITHUB_REPOSITORY;Naios/function2;GIT_TAG;4.2.4")
+set(function2_FOUND TRUE)
---
+++ cmake-build-debug/CPM_modules/Findspdlog.cmake
@@ -0,0 +1,3 @@
+include("C:/Users/79014/CLionProjects/graphics-course/cmake-build-debug/cmake/CPM_0.40.2.cmake")
+CPMAddPackage("NAME;spdlog;GITHUB_REPOSITORY;gabime/spdlog;VERSION;1.13.0;OPTIONS;SPDLOG_FMT_EXTERNAL")
+set(spdlog_FOUND TRUE)
