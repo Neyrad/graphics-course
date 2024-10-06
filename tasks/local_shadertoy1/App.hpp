ddd5133b1457b0b3f5ec9945516cd2b237f1df36
Neyrad <gapandrey328@gmail.com>
2024-10-06 20:07:58 +0300

    Revert "my shader"
    This reverts commit f5b440cd52f1a4c55398a02db35b99fc703c87be.

--- tasks/local_shadertoy1/App.hpp
+++ tasks/local_shadertoy1/App.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
-#include <etna/Sampler.hpp>
 #include <etna/Window.hpp>
 #include <etna/PerFrameCmdMgr.hpp>
 #include <etna/ComputePipeline.hpp>
 #include <etna/Image.hpp>
-#include <chrono>
 
 #include "wsi/OsWindowingManager.hpp"
+
 
 class App
 {
@@ -29,19 +28,4 @@ private:
 
   std::unique_ptr<etna::Window> vkWindow;
   std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
-
-  etna::ComputePipeline pipeline;
-  etna::Image shader_image;
-  etna::Sampler sampler;
-  glm::vec2 mouse_pos;
-  std::chrono::time_point<std::chrono::system_clock> init_time = std::chrono::system_clock::now();
-
-  struct Params
-  {
-    glm::vec2 resolution;
-    glm::vec2 mouse_pos;
-    float time;
-  };
-
-  Params params;
 };
