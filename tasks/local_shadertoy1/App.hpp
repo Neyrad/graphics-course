f5b440cd52f1a4c55398a02db35b99fc703c87be
Neyrad <gapandrey328@gmail.com>
2024-10-06 20:05:50 +0300

    my shader

--- tasks/local_shadertoy1/App.hpp
+++ tasks/local_shadertoy1/App.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <etna/Sampler.hpp>
 #include <etna/Window.hpp>
 #include <etna/PerFrameCmdMgr.hpp>
 #include <etna/ComputePipeline.hpp>
 #include <etna/Image.hpp>
+#include <chrono>
 
 #include "wsi/OsWindowingManager.hpp"
-
 
 class App
 {
@@ -28,4 +29,19 @@ private:
 
   std::unique_ptr<etna::Window> vkWindow;
   std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  etna::ComputePipeline pipeline;
+  etna::Image shader_image;
+  etna::Sampler sampler;
+  glm::vec2 mouse_pos;
+  std::chrono::time_point<std::chrono::system_clock> init_time = std::chrono::system_clock::now();
+
+  struct Params
+  {
+    glm::vec2 resolution;
+    glm::vec2 mouse_pos;
+    float time;
+  };
+
+  Params params;
 };
